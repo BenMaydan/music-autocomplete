6e01cd93570b3216cf83495795bf97b63a1f7290
BenMaydan <maydanb@gmail.com>
2025-11-17 19:29:41 -0600

    added option to use all songs by setting equal to 0 in cmd line arguments

--- dataloader.py
+++ dataloader.py
@@ -155,6 +155,8 @@ def get_dataloaders(config):
     random.shuffle(all_json_paths)
 
     # Select the subset
+    if NUM_SONGS_TO_USE == 0:
+        NUM_SONGS_TO_USE = len(all_json_paths)
     if NUM_SONGS_TO_USE > len(all_json_paths):
         print(f"Warning: You asked for {NUM_SONGS_TO_USE} songs, but only {len(all_json_paths)} were found.")
         NUM_SONGS_TO_USE = len(all_json_paths)
